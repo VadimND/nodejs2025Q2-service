38d5c1fc4767063754c94c10545d7a7d75756f30
Vadim <javadimus@gmail.com>
2025-06-03 00:13:28 +0300

    feat: add 2 modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
 
+const PORT = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const filePath = join(__dirname, '..', 'doc', 'api.yaml');
+  const file = await readFile(filePath, 'utf-8');
+  const swaggerDocument = parse(file);
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
+  await app.listen(PORT);
 }
 bootstrap();
